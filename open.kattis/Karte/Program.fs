a20d4de6d167a2c4eca17433db0ac261dc80f214
LukasA707 <36540127+LukasA707@users.noreply.github.com>
2024-10-31 23:58:54 +0100

    clean up

--- open.kattis/Karte/Program.fs
+++ open.kattis/Karte/Program.fs
@@ -11,29 +11,3 @@ let isGreska : bool = false
 let map : Map<String,bool> = Map<string, bool> []
 
 let check s : string = if s.Length > 3 then check(s.SubString(3)) if map.ContainsKey(s.SubString(0,3)) then isGreska = true else map.Add(s.)
-
-
-// let funcName string =
-// if string.Count > 3
-// do funcName string.subString(4,string.Count)
-// 
-// if (map.contians(string))
-//      isGreska = true
-//  else 
-//      map.add(string)
-//      if (string.charAt(0) == 'P)
-//          P--
-//      ...
-//
-// return 
-// 
-// funcName(PO1HO2KO3)
-//
-// if (isGreska) do printf "Greska" else do printf $"{p} {k} {h} {t}"
-
-
-// PO1H02K03
-
-// funcName(PO1HO2KO3)
-    // funcName(H02K03)
-        //funcName(K03)
